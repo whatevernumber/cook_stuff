6ce4a09cf523526c23ddd1e0b2f3bc3d302414d6
whatevernumber <achford@mail.ru>
2022-07-23 16:38:21 +0300

    randomize side-dishes

--- giveSideDish.js
+++ giveSideDish.js
@@ -1,18 +1,22 @@
 import { HOTSTATIONS } from "./createSideDish.js";
+import { randomize } from "./math.js";
 
 const giveDish = () => {
   try {
-    let availableDish = HOTSTATIONS.querySelector('[data-status = "occupied"]');
-    let dishIndex = availableDish.querySelector(".hs-counter");
-    let portions = Number(dishIndex.textContent);
+    let availableDishes = Array.from(
+      HOTSTATIONS.querySelectorAll('[data-status = "occupied"]')
+    );
 
+    let dish = availableDishes[randomize(0, availableDishes.length - 1, 0)];
+    let dishIndex = dish.querySelector(".hs-counter");
+    let portions = Number(dishIndex.textContent);
     if (portions > 1) {
       dishIndex.textContent = portions - 1;
     } else {
-      availableDish.dataset.status = "empty";
-      availableDish.removeChild(availableDish.querySelector('.hs-dish'));
+      dish.dataset.status = "empty";
+      dish.removeChild(dish.querySelector(".hs-dish"));
     }
-  }  catch {
+  } catch {
     console.log("Нет доступных блюд!");
   }
 };
