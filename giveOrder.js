55ed6a8e5e11e6504c9dc07a1349a23d18b70cfc
the_nepodarok <kittyrat@yandex.ru>
2022-07-23 13:10:10 +0300

    set a custom event on every 'give order'

--- giveOrder.js
+++ giveOrder.js
@@ -1,19 +1,35 @@
 import { callOrders } from './createOrders.js';
-import {randomize} from "./math.js";
+import { spawnCustomer } from './customers.js';
 
-const orderTrays = document.querySelectorAll('.order-tray');
+const orderTrays = Array.from(document.querySelectorAll('.order-tray'));
 
-setTimeout(() => {
-    orderTrays.forEach((tray) => {
-        tray.addEventListener('click', () => {
+const completeEvent = new CustomEvent('give', {
+    detail: {datasetStatus: 'complete'}
+});
 
-        if (tray.dataset.status != 'empty') {
-            try {
-                tray.removeChild(tray.querySelector('.dish'));
-                callOrders();
+const despawnCustomer = (index) => {
+    const customers = document.querySelectorAll('.customer');
+    customers[index].classList.add('customer-leave');
+    setTimeout(() => customers[index].remove(), 1000);
+}
+
+orderTrays.forEach((tray) => {
+        tray.addEventListener('click', (evt) => {
+            if (tray.dataset.status !== 'empty') {
+                try {
+                    const index = orderTrays.indexOf(evt.target);
+                    despawnCustomer(index);
+                    tray.removeChild(tray.querySelector('.dish'));
+                    tray.dataset.status = 'complete';
+                    tray.dispatchEvent(completeEvent);
+                    callOrders(tray);
+                }
+                catch {console.log('Tray empty!');}
             }
-            catch {console.log('Tray empty!');}
-        }
-    })
-})
-}, 1500);
+        });
+
+        tray.addEventListener('give', () => {
+            console.log('You can set an action triggering on this event!');
+        })
+    });
+
